ab1e7a761022a1549abb5165073b5ae506ca8bf9
noman2016 <hafezalnoman@gmail.com>
2022-09-04 13:53:51 +0600

    Add Arithmetic Operator

---
+++ Operators/Arithmetic_Operators_in_Python.py
@@ -0,0 +1,23 @@
+# Arithmetic Operators
+#   +	    Addition	            x + y
+#   -	    Subtraction	            x - y
+#   *	    Multiplication	        x * y
+#   /	    Division	            x / y
+#   %	    Modulus	                x % y
+#   **	    Exponentiation	        x ** y
+#   //	    Floor division	        x // y
+
+x = 10
+y = 2
+
+# z = x + y
+#
+# print(z)
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
