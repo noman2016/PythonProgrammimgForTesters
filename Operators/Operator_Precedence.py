7d70731ed97a38ff5aca9e7c822dc6c40b171dc1
noman2016 <hafezalnoman@gmail.com>
2022-09-27 17:17:58 +0600

    Add Operator_Precedence

---
+++ Operators/Operator_Precedence.py
@@ -0,0 +1,170 @@
+#01
+"""
+(expressions...),
+
+[expressions...], {key: value...}, {expressions...}
+"""
+
+# Binding or parenthesized expression, list display, dictionary display, set display
+
+
+
+#02
+"""
+x[index], x[index:index], x(arguments...), x.attribute
+"""
+
+# Subscription, slicing, call, attribute reference
+
+
+
+#03
+"""
+await x
+"""
+
+# Await expression
+
+
+
+#04
+"""
+**
+"""
+
+# Exponentiation 5
+
+
+
+#05
+"""
++x, -x, ~x
+"""
+
+# Positive, negative, bitwise NOT
+
+
+
+#06
+"""
+*, @, /, //, %
+"""
+
+# Multiplication, matrix multiplication, division, floor division, remainder 6
+
+
+
+#07
+"""
++, -
+"""
+
+# Addition and subtraction
+
+
+
+#08
+"""
+<<, >>
+"""
+
+# Shifts
+
+
+
+#09
+"""
+&
+"""
+
+# Bitwise AND
+
+
+
+#10
+"""
+^
+"""
+
+# Bitwise XOR
+
+
+
+#11
+"""
+|
+"""
+
+# Bitwise OR
+
+
+
+#12
+"""
+in, not in, is, is not, <, <=, >, >=, !=, ==
+"""
+
+# Comparisons, including membership tests and identity tests
+
+
+
+#13
+"""
+not x
+"""
+
+# Boolean NOT
+
+
+
+#14
+"""
+and
+"""
+
+# Boolean AND
+
+
+
+#15
+"""
+or
+"""
+
+# Boolean OR
+
+
+
+#16
+"""
+if – else
+"""
+
+# Conditional expression
+
+
+
+#17
+"""
+lambda
+"""
+
+# Lambda expression
+
+
+
+#18
+"""
+:=
+"""
+
+# Assignment expression
+
+
+#-------------------------------------------------------------------------------------#
+#=====================================================================================#
+
+print(5-8+2)
+print(5+(8-2))
+print(4**2+(4-2))
+print(4**(2+4)-2)
