85e85d5412176c75719684964d5413ef5fbb5b2e
noman2016 <hafezalnoman@gmail.com>
2022-09-29 11:59:02 +0600

    Add Strings File

---
+++ Strings/Strings_in_Python.py
@@ -0,0 +1,22 @@
+x = "This is a string"
+y = "this is 'string' in single quotes"
+z = 'this is "string" in single quotes'
+xy = 'this is \'string\' in single quotes'
+
+yz = """
+This is a
+Multi Line Comments
+"""
+zx = '''
+This is also Multiline comments
+'''
+
+print(x)
+print(y)
+print(z)
+print(xy)
+print(y[5])
+print(yz)
+print(zx)
+print("is" in zx)
+print("iss" in yz)
